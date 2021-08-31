3404e68df3dfd7ef7607ba699a95a274bd66ff9d
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-08-31 16:11:52 +0000

    doing for print statements

---
+++ print_statements.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo hello world
+
+echo -e "line1\nline2"
+
+echo -e "word1\tword2"
+
+echo -e "\e[31mredcolour"
+echo -e "\e[32mgreencolour"
+
+echo -e "\e[1;31mText in red colour"
---
+++ sample.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ls -l
+cat /etc/*release
+
+# any line started with # is treated as comment it will be ignored by the interpreter
