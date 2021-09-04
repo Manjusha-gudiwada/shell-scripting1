103c8b4e2ae7496e056f2ce9f47c57d3dc77406d
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-04 13:54:30 +0000

    pushing catalogue code

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -7,19 +7,18 @@ print "install yum utils and download redis repos"
 yum install epel-release yum-utils -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>LOG
 Status_Check $?
 
-print "setting up redis repos"
+print "setting up redis repos\t\t\t"
 yum-config-manager --enable remi &>>$LOG
 Status_Check $?
 
-print "install redis repos"
+print "install redis repos\t\t\t"
 yum install redis -y &>>$LOG
 Status_Check $?
 
-print "configure redis ip adress"
+print "configure redis ip adress\t\t"
 sed -i -e  's/127.0.0.1/0.0.0.0/'  /etc/redis.conf 
 Status_Check $?
 
-print "starting database"
- 
+print "starting database\t\t\t"
 systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
 Status_Check $?
