507b9f4b4aa950112aa30b3ae11e98cb1bd7b8d1
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-04 13:05:56 +0000

    pushing catalogue code

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -37,5 +37,5 @@ Status_Check $?
 chown roboshop:roboshop -R /home/roboshop
 
 print "setup systemd service"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl start catalogue &>>$LOG && systemctl enable catalogue
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl start catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
 Status_Check $?
