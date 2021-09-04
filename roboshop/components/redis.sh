e7e5b54fa34e9f3d539b72d62247f2841365351e
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-04 13:53:24 +0000

    pushing catalogue code

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -20,6 +20,6 @@ sed -i -e  's/127.0.0.1/0.0.0.0/'  /etc/redis.conf
 Status_Check $?
 
 print "starting database"
-Start Redis Database
+ 
 systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
 Status_Check $?
