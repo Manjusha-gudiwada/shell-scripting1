594faff8f0126fb0e0b95b60d704872be4d5d649
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-06 20:41:30 +0530

    pushing create-instance

--- create-instance.sh
+++ create-instance.sh
@@ -3,4 +3,4 @@
 LID="lt-0fb3ba0708e861323"
 LVER=2
 
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER
+aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER \ jq
