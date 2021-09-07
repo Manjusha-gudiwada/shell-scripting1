12e52fb25c5a6b0a23132ea89603602d834f62c9
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-07 17:08:55 +0530

    pushing create-instance

--- create-instance.sh
+++ create-instance.sh
@@ -18,5 +18,12 @@ echo "Instance $Instance_Name is already running"
 exit 0
 fi
 
+aws ec2 describe-instances --filters "Name=tag:Name,Values=$Instance_Name" | jq .Reservations[].Instances[].State.Name | grep stopped &>/dev/null
+
+if [ $? -eq 0 ]; then
+echo "Instance $Instance_Name is already created & stopped"
+exit 0
+fi
+
 Ip=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Instance_Name}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$Instance_Name}]" | jq .Instances[].PrivateIpAddress) | sed -e 's/"//g'
     
