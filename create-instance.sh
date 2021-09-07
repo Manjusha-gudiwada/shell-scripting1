859cc22c34d98ab9d28904c2b667f2c31b0814ac
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-07 16:59:11 +0530

    pushing create-instance

--- create-instance.sh
+++ create-instance.sh
@@ -11,5 +11,12 @@ echo "Input is missing"
 exit 1
 fi
 
+aws ec2 describe-instances --filters "Name=tag:Name,Values=$Instance_Name" | jq .Reservations[].Instances[].State.Name | grep running &>/dev/null
+
+if [ $? -eq 0 ]; then
+echo "Instance $Instance_Name is already running"
+exit 0
+fi
+
 Ip=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Instance_Name}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$Instance_Name}]" | jq .Instances[].PrivateIpAddress) | sed -e 's/"//g'
     
