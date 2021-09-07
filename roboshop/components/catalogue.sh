9d4be3d338a323e2e439ddf83c1b9ff27df508d9
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-07 20:57:25 +0530

    updating common.sh file

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,46 +2,5 @@
 
 source components/common.sh
 
-print "Installing nodejs"
-
-yum install nodejs make gcc-c++ -y &>>$LOG
-Status_Check $?
-
-
-
-print "adding roboshop user"
-id roboshop &>>$LOG
-if [ $? -eq 0 ]; then
-echo "user exists so skipping this step" &>>$LOG
-
-else
-useradd roboshop &>>$LOG
-fi
-Status_Check $?
-
-print "downloading catalogue"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-Status_Check $?
-
-print "Extracting catalogue"
-cd /home/roboshop
-# in order to avoid many times unzip of file we are removing content inside it before running
-rm -rf catalogue && unzip /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue
-Status_Check $?
-
-print "downloading nodejs dependencies"
-cd /home/roboshop/catalogue
-npm install --unsafe-perm &>>$LOG # this is written becoz we have to switch to user and install
-Status_Check $?
-
-chown roboshop:roboshop -R /home/roboshop
-
-print "update systemd service"
-
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
-Status_Check $?
-
-print "setup systemD service file"
-
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl restart catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
-Status_Check $?
+component=catalogue
+NODEJS
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -26,3 +26,66 @@ Status_Check()
 
 LOG=/tmp/roboshop.log
 rm -f $LOG
+
+ADD_APP_USER() {
+
+print "adding roboshop user\t"
+id roboshop &>>$LOG
+if [ $? -eq 0 ]; then
+echo "user exists so skipping this step" &>>$LOG
+
+else
+useradd roboshop &>>$LOG
+fi
+Status_Check $?
+
+}
+
+DOWNLOAD() {
+print "downloading ${component} "
+curl -s -L -o /tmp/${component}.zip "https://github.com/roboshop-devops-project/${component}/archive/main.zip"
+Status_Check $?
+
+print "Extracting ${component}"
+cd /home/roboshop
+# in order to avoid many times unzip of file we are removing content inside it before running
+rm -rf ${component} && unzip /tmp/${component}.zip &>>$LOG && mv ${component}-main ${component}
+Status_Check $?
+
+}
+
+SystemD-Setup() {
+
+  print "update systemd service"
+
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/${component}/systemd.service
+Status_Check $?
+
+print "setup systemD service file"
+
+mv /home/roboshop/${component}/systemd.service /etc/systemd/system/${component}.service && systemctl daemon-reload && systemctl restart ${component} &>>$LOG && systemctl enable ${component} &>>$LOG
+Status_Check $?
+
+}
+
+NODEJS() {
+
+print "Installing nodejs"
+
+yum install nodejs make gcc-c++ -y &>>$LOG
+Status_Check $?
+
+ADD_APP_USER
+DOWNLOAD
+
+print "downloading nodejs dependencies"
+cd /home/roboshop/${component}
+npm install --unsafe-perm &>>$LOG # this is written becoz we have to switch to user and install
+Status_Check $?
+
+chown roboshop:roboshop -R /home/roboshop
+
+SystemD-Setup
+
+
+}
