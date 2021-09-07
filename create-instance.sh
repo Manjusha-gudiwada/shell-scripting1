fc72891d79dc098828f7b6c47733c38a995d7b6b
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-07 14:26:27 +0530

    pushing create-instance

--- create-instance.sh
+++ create-instance.sh
@@ -2,4 +2,11 @@
 
 LID="lt-0fb3ba0708e861323"
 LVER=2
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER | jq .Instances[].InstanceId
+Instance_Name=$1
+
+if [-z "${Instance_Name}" - ]; then
+echo "Input is missing"
+exit 1
+fi
+
+Ip=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Instance_Name} ""ResourceType=spot-instances-request,Tags=[{Key=Name,Value=Instance_Name}" " | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
