f7ffb50ce305d7d1378d74c0911adead74bf0dd9
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-07 19:58:40 +0530

    pushing create-instance

---
+++ roboshop/components/frontend.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source components/common.sh
+
+
+print "installing nginx\t\t"
+yum install nginx -y &>>$LOG
+Status_Check $?
+
+print "downloading frontend archive"
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+Status_Check $?
+
+print "Extract frontend archive"
+rm -rf /usr/share/nginx/* && cd /usr/share/nginx/html &&  unzip -o /tmp/frontend.zip && mv frontend-main/* . && mv static/* .  &>>$LOG
+Status_Check $?
+
+print "update nginx Roboshop confg file" 
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+Status_Check $?
+
+print "restart nginx\t\t"
+
+systemctl restart nginx && systemctl enable nginx &>>$LOG
+Status_Check $?
