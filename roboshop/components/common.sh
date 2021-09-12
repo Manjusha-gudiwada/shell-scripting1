37a8bc70967aebc81cb5bd476abf4cbf44da227d
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-12 22:58:23 +0530

    updating common file

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -58,7 +58,7 @@ SystemD-Setup() {
 
   print "update systemd service"
 
- sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e  's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+ sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e  's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${component}/systemd.service
 Status_Check $?
 
 print "setup systemD service file"
