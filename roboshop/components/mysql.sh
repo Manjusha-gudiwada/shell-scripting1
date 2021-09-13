be61b245fcab8dbeffaa0669d4d2f305d73c92c7
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-13 20:21:41 +0530

    updating mysql file

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -3,7 +3,7 @@
 source components/common.sh
 
 
-print "setup mysql"
+print "setup mysql\t\t"
 echo '[mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
@@ -11,18 +11,18 @@ enabled=1
 gpgcheck=0' > /etc/yum.repos.d/mysql.repo
 Status_Check $?
 
-print "Install MySQL"
+print "Install MySQL\t\t"
 yum remove mariadb-libs -y &>>$LOG && yum install mysql-community-server -y &>>$LOG
 Status_Check $?
 
-print "Start MySQL"
+print "Start MySQL\t\t"
 systemctl enable mysqld &>>$LOG &&  systemctl start mysqld &>>$LOG
 Status_Check $?
 
 
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
- print "changing default password"
+ print "changing default password\t"
 echo 'show databases' | mysql -uroot -pRoboShop@1 &>>$LOG
 
 if [ $? -eq 0 ]; then
@@ -41,14 +41,14 @@ mysql -u root -p"RoboShop@1" </tmp/pass.sql &>>$LOG
 Status_Check $?
 
 
-print "downloading schema"
+print "downloading schema\t"
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG
 Status_Check $?
 
-print "extract schema file"
+print "extract schema file\t"
 cd /tmp && unzip -o mysql.zip &>>$LOG
 Status_Check $?
 
-print "loading schema"
+print "loading schema\t\t"
 cd mysql-main && mysql -u root -pRoboShop@1 <shipping.sql &>>$LOG
 Status_Check $?
