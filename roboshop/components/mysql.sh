71e6be52bf818ef07e46446329150bd63475ad65
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-09-13 20:35:15 +0530

    updating mysql file

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -35,12 +35,13 @@ Status_Check $?
 
 
 print "uninstall password validate_plugin"
-echo 'show plugins;' |  mysql -u root -p"RoboShop@1" | grep -i validate_password &>>$LOG
+echo 'show plugins;' | mysql -u root -p"RoboShop@1" | grep -i validate_password &>>$LOG
 if [ $? -eq 0]; then
 echo "uninstall plugin validate_password;" >/tmp/pass.sql
 mysql -u root -p"RoboShop@1" </tmp/pass.sql &>>$LOG
 else
 echo "password plugin is already uninstalled" &>>$LOG
+fi
 Status_Check $?
 
 
